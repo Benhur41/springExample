4985d801da324f1a9d6defffeb08710a76c80f19
rlaxodus <hbj04003@gmail.com>
2023-06-21 16:49:30 +0900

    dsds

---
+++ 230608/src/main/java/com/yedam/app/user/service/UserVO.java
@@ -0,0 +1,80 @@
+package com.yedam.app.user.service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserVO implements UserDetails {
+	
+	//private MemberVO vo; 보통 내장형 객체사용함 jacksonDatabind 방식으로 호환되지않는 타입존재하기때문 
+	
+	private String id;
+	private String pwd;
+	private String role;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> auth = new ArrayList<>(); 
+		auth.add(new SimpleGrantedAuthority(this.role));
+		return auth;
+	}
+
+	@Override
+	public String getPassword() {
+		return pwd;
+	}
+
+	@Override
+	public String getUsername() {
+		return id;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
